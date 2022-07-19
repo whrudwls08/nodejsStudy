ceafeadc0f71f4fb8bcd8680a60a554b81ae8021
DESKTOP-S5M2HJU\Jo <whrudwls08@naver.com>
2022-07-19 15:59:35 +0900

    1 express 04 ViewEngine

--- node-express/routes/admin.js
+++ node-express/routes/admin.js
@@ -6,7 +6,10 @@ router.get('/', (req, res) => {
 })
 
 router.get('/products', (req, res) => {
-    res.send('admin products');
-})
+    res.render( 'admin/products.html',{
+        message: 'hello!!!!!!!!!!',
+        online: 'express'
+    })
+});
 
 module.exports = router;
