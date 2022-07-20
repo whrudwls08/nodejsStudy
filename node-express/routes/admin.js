e319b63cb1f02d3af2848ddccf8322d1f85882a0
DESKTOP-S5M2HJU\Jo <whrudwls08@naver.com>
2022-07-20 10:32:29 +0900

    1 express 06 middleware

--- node-express/routes/admin.js
+++ node-express/routes/admin.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('admin 이후 url');
+function testMiddleware(req, res, next) {
+	console.log('첫번쨰 미들웨어');
+	next();
+}
+
+function testMiddleware2(req, res, next) {
+	console.log('두번쨰 미들웨어');
+	next();
+}
+
+router.get('/', testMiddleware, testMiddleware2, (req, res) => {
+	res.send('admin 이후 url');
 })
 
 router.get('/products', (req, res) => {
-    res.render( 'admin/products.html',{
-        message: `<h1>태그가 출력됩니다.</h1>`,
-        online: 'express'
-    })
-});
 
+	res.render('admin/products.html', {
+		message: `<h1>태그가 출력됩니다.</h1>`,
+		online: 'express'
+	})
+});
 module.exports = router;
