6b4604450003008bae62d6f46d1b2517df7fbbd1
DESKTOP-S5M2HJU\Jo <whrudwls08@naver.com>
2022-09-27 14:44:14 +0900

    addProduct 추가

--- node-express/start.js
+++ node-express/start.js
@@ -1,47 +1,56 @@
-const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-
-    type Product {
-      id: ID! 
-      name : String
-      price : Int
-      description : String
+    input ProductInput {
+        name: String
+        price: Int
+        description : String
     }
-
+    type Product {
+        id: ID!
+        name: String
+        price: Int
+        description : String
+    }
     type Query {
-        getProduct( id : ID! ) : Product
+        getProduct ( id: ID! ): Product
+    }
+    type Mutation {
+        addProduct(input: ProductInput): Product
     }
 `);
 
 const products = [{
-  id : 1,
-  name : '첫번째 제품',
-  price : 2000,
-  description : '안녕'
+    id: 1,
+    name: '첫번째 제품',
+    price: 2000,
+    description : "하하하"
 },{
-  id : 2,
-  name : '두번째 제품',
-  price : 4000,
-  description : '잘가'
+    id: 2,
+    name: '두번째 제품',
+    price: 1200,
+    description : "호호호"
 }]
-const root = {
-  getProduct : ({ id }) => products.find( product => product.id === parseInt(id))
+
+const root = { 
+    getProduct : ({id}) => products.find( product => product.id === parseInt(id) ) ,
+    addProduct :  ({input}) => {
+        input.id = parseInt(products.length+1);
+        products.push(input);
+        return root.getProduct({id : input.id});
+    },
 };
 
 const app = express();
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(4000, () => {
-  console.log("Running a GraphQL API server at localhost:4000/graphql");
-});
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
+}); 
